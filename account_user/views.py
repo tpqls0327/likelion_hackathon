adde968824e300b8edc35b4cc6fe3cffd0f4879b
dhdlrlgns2 <dhdlrlgns@likelion.org>
2019-08-04 16:10:16 +0900

    정확한 주소를 위한 검색버튼 추가

--- account_user/views.py
+++ account_user/views.py
@@ -2,8 +2,49 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+#def signin(request):
+#    return render(request, 'accounts/signin.html')
+
+#def signup(request):
+#    return render(request, 'accounts/signup.html')
+
 def signin(request):
-    return render(request, 'accounts/signin.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'accounts/signin.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/signin.html')
 
 def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('/')
     return render(request, 'accounts/signup.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('/')
+    return render(request, 'accounts/signup.html')
+
+def kakao_login(request, test):
+    idd = test.split("%20")[0]
+    name = test.split("%20")[1]
+    email = test.split("%20")[2]
+    user = auth.authenticate(request, username=email, password=idd, email = name)
+    if user is not None:
+        auth.login(request, user)
+        return redirect('/')
+    else :
+        User.objects.create_user(user)
+        auth.login(request, user)
+        return redirect('/')
