83c2de7917ee6e7f5ca12462d497dc143fe548d6
dhdlrlgns2 <dhdlrlgns@likelion.org>
2019-08-08 22:45:30 +0900

    error2

--- account_user/forms.py
+++ account_user/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ("username","password")
+User = get_user_model()
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        class_update_fields = ['username', 'password']
+        for field_name in class_update_fields:
+            self.fields[field_name].widget.attrs.update({
+                'class': 'form-control'
+            })
